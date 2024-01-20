b0d873ced31d16a1e7902ec33a6026ec6a863641
StevenWin818 <awen0818@hotmail.com>
2024-01-20 20:46:08 +0800

    更新

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use std::io;
+fn main() {
+    println!("----猜数----");
+
+    println!("猜测一个数：");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).expect("无法读取行");
+
+    println!("你猜的数字是:{}",guess);
+}
